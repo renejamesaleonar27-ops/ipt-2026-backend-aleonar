da99b3cbba1a4324412d03a7fbcf3e6fb48f790a
renejamesaleonar27-ops <renejamesaleonar27@gmail.com>
2026-05-22 21:44:07 +0800

    Update swagger.ts

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -3,6 +3,16 @@ const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger.json';
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const CSS_URL = "https://unpkg.com/swagger-ui-dist@5.32.4/swagger-ui.css";
+const JS_URLS = [
+    "https://unpkg.com/swagger-ui-dist@5.32.4/swagger-ui-bundle.js",
+    "https://unpkg.com/swagger-ui-dist@5.32.4/swagger-ui-standalone-preset.js"
+];
+
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCssUrl: CSS_URL,
+    customJs: JS_URLS
+}));
 
 export default router;
+unpkg.com
